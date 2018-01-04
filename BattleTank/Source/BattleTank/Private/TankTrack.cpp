856d7d621dfe175cd1dc788479a5a31371aec89d
Clint-PC\Clint <clint.brewer@thethursdays.org>
2018-01-03 19:39:20 -0600

    Currently Not Working

--- BattleTank/Source/BattleTank/Private/TankTrack.cpp
+++ BattleTank/Source/BattleTank/Private/TankTrack.cpp
@@ -2,6 +2,12 @@
 
 #include "TankTrack.h"
 
+void UTankTrack::SetThrottle(float Throttle)
+{
+	//auto Time = GetWorld()->GetTimeSeconds();
+	auto Name = GetName();
+	UE_LOG(LogTemp, Warning, TEXT("%s throttle: %f"), *Name, Throttle);
 
+}
 
 
--- BattleTank/Source/BattleTank/Public/TankTrack.h
+++ BattleTank/Source/BattleTank/Public/TankTrack.h
@@ -7,14 +7,16 @@
 #include "TankTrack.generated.h"
 
 /**
- * 
+ * TankTrack is used to set maximum driving force and to apply forces to the tank.
  */
 UCLASS(meta = (BlueprintSpawnableComponent))
 class BATTLETANK_API UTankTrack : public UStaticMeshComponent
 {
 	GENERATED_BODY()
 	
-	
+public:
+	UFUNCTION(BlueprintCallable, Category=Input)
+	void SetThrottle(float Throttle);
 	
 	
 };
